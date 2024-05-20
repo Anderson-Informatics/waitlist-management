1649bb39f89f8f46109d5b2c955f39caf9295879
Eric Anderson <eric@booksuey.com>
2024-05-20 17:04:41 +0200

    removed user server middleware

--- server/middleware/user.ts
+++
@@ -1,6 +0,0 @@
-import { serverSubabaseUser } from "#supabase/server";
-
-export default defineEventHandler(async (event) => {
-  const user = await serverSubabaseUser(event);
-  event.context.user = user;
-});
